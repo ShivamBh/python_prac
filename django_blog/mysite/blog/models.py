10e9d1ca98eb24e66f7dfa3c06f871f4a2e05d4c
ShivamBh <shivam.bhattacharjee94@gmail.com>
2017-03-03 05:44:46 +0530

    model manager added, view defined

--- django_blog/mysite/blog/models.py
+++ django_blog/mysite/blog/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class PublishedManager(models.Manager):
+	def get_queryset(self):
+		return super(PublishedManager, self).get_queryset()\
+											.filter(status='published')
 
 class Post(models.Model):
 	STATUS_CHOICES = (
@@ -14,14 +18,19 @@ class Post(models.Model):
 	title = models.CharField(max_length=250)
 	slug = models.SlugField(max_length=250, unique_for_date='publish')
 	author = models.ForeignKey(User, related_name='blog_posts')
-	body = models.Textfield()
+	body = models.TextField()
 	publish = models.DateTimeField(default=timezone.now)
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
 	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
 	class Meta:
-		ordering = ('-publish')
+		ordering = ('-publish',)
 
 	def __str__(self):
+		
 		return self.title
+
+	objects = models.Manager() #default manager
+	published = PublishedManager() #custom manager
+
